694716130bd2cadd56273581dd13edd95c41588b
RONIN <es-omar.wael2027@alexu.edu.eg>
2025-08-03 20:29:06 +0300

    limited-attempts-feature
    added limited attempts for some tactics

--- f1/Tasks/src/task1.3/tactics/aggressive_block.py
+++ f1/Tasks/src/task1.3/tactics/aggressive_block.py
@@ -1,6 +1,9 @@
 from .tactic import Tactic 
 
 class AggressiveBlock(Tactic):
+    def __init__(self):
+        super().__init__()
+        self.attemps = 2
     @property
     def name(self):
         return "Aggressive Block"
@@ -10,7 +13,13 @@ class AggressiveBlock(Tactic):
         return 35
     
     def execute(self,driver, opponent):
+        if self.attemps <= 0:
+            print(f"{driver.name} has no attempts left for {self.name}.\n skipping turn.")
+            return
         driver.fuel -= self.fuel_cost
         driver.tire_health += driver.turn_damage
         opponent.turn_damage = 0
+        self.attemps -= 1
+        print(f"{self.name} remains active. {self.attemps} attempts left.")
+
         
--- f1/Tasks/src/task1.3/tactics/ers_deployment.py
+++ f1/Tasks/src/task1.3/tactics/ers_deployment.py
@@ -1,6 +1,9 @@
 from .tactic import Tactic 
 
 class ErsDeployment(Tactic):
+    def __init__(self):
+        super().__init__()
+        self.attemps = 3
     @property
     def name(self):
         return "ERS Deployment"
@@ -10,6 +13,11 @@ class ErsDeployment(Tactic):
         return 40
     
     def execute(self,driver,opponent):
+        if self.attemps <= 0:
+            print(f"{driver.name} has no attempts left for {self.name}.\n skipping turn.")
+            return
         driver.fuel -= self.fuel_cost
         driver.tire_health += .5*driver.turn_damage
         opponent.turn_damage = 0
+        self.attemps -= 1
+        print(f"{self.name} remains active. {self.attemps} attempts left.")
